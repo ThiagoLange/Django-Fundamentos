67608317859a026efde2ff1f18ac6d8dcb958799
Thiago <thiago_lange@yahoo.com.br>
2020-03-05 16:29:00 -0300

    Exibindo Cliente por id

--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Cliente
 from .forms import ClienteForm
 # Create your views here.
@@ -12,6 +13,11 @@ def inserir_cliente(request):
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('listar_clientes')
     else:
         form = ClienteForm()
     return render(request, 'clientes/form_cliente.html', {'form': form})
+
+def listar_cliente_id(request, id):
+    cliente = Cliente.objects.get(id=id)
+    return render(request, 'clientes/lista_cliente.html', {'cliente': cliente})
